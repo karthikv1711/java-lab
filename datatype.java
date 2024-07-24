5b60d6f5eafc5c9ba134b8fa70377229b19bf9fd
karthikv1711 <164615028+karthikv1711@users.noreply.github.com>
2024-07-24 11:38:34 +0530

    Add files via upload
    size of data types in java

---
+++ datatype.java
@@ -0,0 +1,23 @@
+public class datatype {
+	static boolean 	defaultBoolean;
+	static byte 	defaultByte;
+	static char 	defaultChar;
+	static short 	defaultShort;
+	static int 	defaultInt;
+	static long 	defaultLong;
+	static float	defaultFloat;
+	static double	defaultDouble;
+	
+	public static void main(String[] args){
+		System.out.println("Boolean	:"+defaultBoolean);
+		System.out.println("Byte	:"+defaultByte);
+		System.out.println("Character	:"+defaultChar);
+		System.out.println("Short	:"+defaultShort);
+		System.out.println("Integer	:"+defaultInt);
+		System.out.println("Long	:"+defaultLong);
+		System.out.println("Float	:"+defaultFloat);
+		System.out.println("Double	:"+defaultDouble);
+
+	}
+
+}
